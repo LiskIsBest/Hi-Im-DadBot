03d1ac77524153367fd76c3af692080f6e75cc52
Darien Moore <Darienmoore2002@gmail.com>
2023-02-22 21:30:04 -0600

    removed console log

--- src/events/speech.js
+++ src/events/speech.js
@@ -8,8 +8,7 @@ module.exports = {
   name: "speech",
   execute(msg) {
     if (!msg.content) return;
-    console.log(msg.content);
-
+    
     let regex = /(I'm|I\sam)(.*)/
     const found = msg.content.match(regex)
     if (found){
