446853dc0fad3dea3105d0de635cb92eaedd800b
Darien Moore <Darienmoore2002@gmail.com>
2023-02-24 06:30:15 -0600

    renamed dbConnection to connection

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -10,13 +10,13 @@ module.exports = {
   name: Events.GuildCreate,
   async execute(guild) {
     const guild_id = guild.id
-    const dbConnection = createMysqlConnection();
-    const in_server = await isInServersTable(dbConnection, guild_id);
+    const connection = createMysqlConnection();
+    const in_server = await isInServersTable(connection, guild_id);
     if (!in_server){
-      await insertIntoServers(dbConnection, guild_id);
+      await insertIntoServers(connection, guild_id);
     }
-    await updateInServer(dbConnection, guild_id, true);
-    dbConnection.end();
+    await updateInServer(connection, guild_id, true);
+    connection.end();
     console.log(`UPDATED servers.in_server TO true FOR guild_id = ${guild_id}`);
   },
 };
--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -10,10 +10,10 @@ module.exports = {
   name: Events.GuildDelete,
   async execute(guild) {
     const guild_id = guild.id
-    const dbConnection = createMysqlConnection();
-    const in_server = await isInServersTable(dbConnection, guild_id);
-    await updateInServer(dbConnection, guild_id, false);
-    dbConnection.end();
+    const connection = createMysqlConnection();
+    const in_server = await isInServersTable(connection, guild_id);
+    await updateInServer(connection, guild_id, false);
+    connection.end();
     console.log(`UPDATED servers.in_server TO false FOR guild_id = ${guild_id}`);
   },
 };
