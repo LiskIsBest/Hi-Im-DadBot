ec16e5f3460bde28b0347213826b4d9a5c234634
Darien Moore <Darienmoore2002@gmail.com>
2023-02-24 05:15:33 -0600

    Update speech.js

--- src/events/speech.js
+++ src/events/speech.js
@@ -1,20 +1,28 @@
-const {getVoiceConnection, AudioPlayer, createAudioResource, StreamType} = require("@discordjs/voice");
+const {
+  getVoiceConnection,
+  AudioPlayer,
+  createAudioResource,
+  StreamType,
+} = require("@discordjs/voice");
 const { getVoiceStream } = require("discord-tts");
-require("dotenv").config()
+require("dotenv").config();
 
 module.exports = {
   name: "speech",
   execute(msg) {
     if (!msg.content) return;
-    
-    let regex = /(I'm|I\sam)(.*)/
-    const found = msg.content.match(regex)
-    if (found){
+
+    let regex = /(I'm|I\sam)(.*)/;
+    const found = msg.content.match(regex);
+    if (found) {
       const voiceConnection = getVoiceConnection(msg.guild.id);
-          
+
       const audioPlayer = new AudioPlayer();
       const stream = getVoiceStream(`Hi ${found[2]}, I'm dad.`);
-      const audioResource = createAudioResource(stream, {inputType: StreamType.Arbitrary, inlineVolume:true});
+      const audioResource = createAudioResource(stream, {
+        inputType: StreamType.Arbitrary,
+        inlineVolume: true,
+      });
       voiceConnection.subscribe(audioPlayer);
       audioPlayer.play(audioResource);
     } else {
