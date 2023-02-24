852df751eed5160f0bb45a563e3ee09d21b6c6d0
LiskIsBest <darienmoore2002@gmail.com>
2023-02-24 00:31:49 -0600

    changed from class for database to just functions

--- src/database.js
+++ src/database.js
@@ -5,68 +5,76 @@ function unixtime() {
   return Math.floor(new Date().getTime() / 1000);
 }
 
-class DataBaseConnection {
-  constructor() {
-    this.connection = this.createMysqlConnection();
-  }
-  createMysqlConnection() {
-    return mysql.createConnection({
-      host: process.env.MYSQLHOST,
-      user: process.env.MYSQLUSER,
-      password: process.env.MYSQLPASSWORD,
-      database: process.env.MYSQLDATABASE,
-      port: process.env.MYSQLPORT,
-    });
-  }
-  isInServers(guildId) {
-    var check;
-    this.connection.connect();
-    this.connection.query(
-      `SELECT EXISTS(SELECT * FROM servers WHERE guild_id = ${guildId})`,
-      (error, results, fields) => {
-        if (error) console.error(error);
-        if (results.values()[0] == undefined){
-          check = false;
-        } else {
-          check = true;
-        }
-        this.connection.end();
-      }
-    );
-    return check;
-  }
-  insertIntoServers(guildId) {
-    this.connection.connect();
-    this.connection.query(
-      `INSERT INTO servers (guild_id, date_added, whitelisted, blacklisted, in_server)
-       VALUES (${guildId}, FROM_UNIXTIME(${unixtime()}), false, false, false)`,
-      (error, results, fields) => {
-        if (error) console.error(error);
-        this.connection.end();
-      }
-    );
-  }
-  setInServer(guildId, val) {
-    this.connection.connect();
-    this.connection.query(
-      `UPDATE servers
-       SET in_server = ${val}
-       WHERE guild_id = ${guildId}`,
-      (error, results, fields) => {
-        if (error) console.error(error);
-        this.connection.end();
-      }
-    );
-  }
+function createMysqlConnection() {
+  return mysql.createConnection({
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT,
+    rowsAsArray: true,
+    supportBigNumbers: true,
+  });
 }
 
-const guild_id = 884730247466090546;
+function isInServersTable(connection, guildId) {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      "SELECT * FROM servers WHERE guild_id = ?",
+      [guildId],
+      (error, results) => {
+        return error ? reject(error) : resolve(results[0]);
+      }
+    );
+  });
+}
 
-const dbObject = new DataBaseConnection();
-// dbObject.insertIntoServers(guild_id);
+function insertIntoServers(connection, guildId) {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      `INSERT INTO servers (guild_id, date_added, has_whitelist, has_blacklist, in_server)
+         VALUES (?, FROM_UNIXTIME(?), false, false, false)`,
+      [guildId, unixtime()],
+      (error, results) => {
+        if (error) console.error(error);
+        return error ? reject(error) : resolve(results);
+      }
+    );
+  });
+}
 
-console.log(dbObject.isInServers(guild_id));
+function deleteFromServers(connection, guildId){
+  return new Promise((resolve, reject)=>{
+    connection.query(
+      `DELETE FROM servers WHERE guild_id = ?`,
+      [guildId],
+      (error, results)=>{
+        if (error) console.error(error);
+        return error ? reject(error) : resolve(results);
+      }
+    )
+  })
+}
+
+function updateInServer(connection, guildId, val) {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      `UPDATE servers
+       SET in_server = ${val}
+       WHERE guild_id = ?`,
+      [guildId],
+      (error, results) => {
+        if (error) console.error(error);
+        return error ? reject(error) : resolve(results);
+      }
+    );
+  });
+}
 
 module.exports = {
-  DataBaseConnection,
+  createMysqlConnection,
+  isInServersTable,
+  insertIntoServers,
+  deleteFromServers,
+  updateInServer,
 };
