68993d8a384f281f83e8bea63b4ad0cf21a80764
Darien Moore <Darienmoore2002@gmail.com>
2023-02-23 22:10:56 -0600

    added not in call check

--- src/commands/dadleave.js
+++ src/commands/dadleave.js
@@ -6,9 +6,23 @@ module.exports = {
     .setName("dadleave")
     .setDescription("Hi-Im-dad-bot leaves the voice call!"),
     async execute(interaction){
-      await interaction.reply({ content: "Leaving voice chat!", ephemeral: true });
       const voiceChannel = interaction.member?.voice.channel;
       const voiceConnection = getVoiceConnection(interaction.guildId)
+      if (!voiceConnection) {
+        await interaction.reply({
+          content: "Hi-Im-DadBot is not in a voice call.",
+          ephemeral: true,
+        })
+      }
+
+      if (voiceChannel.id !== voiceConnection.joinConfig.channelId){
+        await interaction.reply({
+          content: "You must be in the same voice call to use /dadleave.",
+          ephemeral: true,
+        })
+      }
+      
+      await interaction.reply({ content: `Leaving ${voiceChannel.name}` });
       voiceConnection.destroy()
     }
 }
