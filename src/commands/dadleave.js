4decac7290613ffff9b1a78149b4e6ac908b79a7
Darien Moore <Darienmoore2002@gmail.com>
2023-02-24 05:21:35 -0600

    Update dadleave.js

--- src/commands/dadleave.js
+++ src/commands/dadleave.js
@@ -1,30 +1,30 @@
-const { SlashCommandBuilder} = require("discord.js");
+const { SlashCommandBuilder } = require("discord.js");
 const { getVoiceConnection } = require("@discordjs/voice");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("dadleave")
     .setDescription("Hi-Im-dad-bot leaves the voice call!"),
-    async execute(interaction){
-      const voiceChannel = interaction.member?.voice.channel;
-      const voiceConnection = getVoiceConnection(interaction.guildId)
-      if (!voiceConnection) {
-        await interaction.reply({
-          content: "Hi-Im-DadBot is not in a voice call.",
-          ephemeral: true,
-        })
-        return;
-      }
-
-      if (voiceChannel.id !== voiceConnection.joinConfig.channelId){
-        await interaction.reply({
-          content: "You must be in the same voice call to use /dadleave.",
-          ephemeral: true,
-        })
-        return;
-      }
-      
-      await interaction.reply({ content: `Leaving ${voiceChannel.name}` });
-      voiceConnection.destroy()
+  async execute(interaction) {
+    const voiceChannel = interaction.member?.voice.channel;
+    const voiceConnection = getVoiceConnection(interaction.guildId);
+    if (!voiceConnection) {
+      await interaction.reply({
+        content: "Hi-Im-DadBot is not in a voice call.",
+        ephemeral: true,
+      });
+      return;
     }
-}
+
+    if (voiceChannel.id !== voiceConnection.joinConfig.channelId) {
+      await interaction.reply({
+        content: "You must be in the same voice call to use /dadleave.",
+        ephemeral: true,
+      });
+      return;
+    }
+
+    await interaction.reply({ content: `Leaving ${voiceChannel.name}` });
+    voiceConnection.destroy();
+  },
+};
