0f60d81001d4bedf399c1ea941e8c2bdf16f077d
Darien Moore <Darienmoore2002@gmail.com>
2023-02-23 22:10:45 -0600

    added already in call check

--- src/commands/dadjoin.js
+++ src/commands/dadjoin.js
@@ -1,20 +1,29 @@
 const { SlashCommandBuilder } = require("discord.js");
-const { joinVoiceChannel } = require("@discordjs/voice");
+const { joinVoiceChannel, getVoiceConnection } = require("@discordjs/voice");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("dadjoin")
     .setDescription("Hi-Im-dad-bot joins the voice call!"),
-    async execute(interaction){
-      await interaction.reply({ content: "Joining voice chat!", ephemeral: true });
-      const voiceChannel = interaction.member?.voice.channel;
-
-      const voiceConnection = joinVoiceChannel({
-        channelId: voiceChannel.id,
-        guildId: interaction.guildId,
-        adapterCreator: interaction.guild.voiceAdapterCreator,
-        selfDeaf: false,
-        selfMute: false,
-      }); 
+  async execute(interaction) {
+    if (getVoiceConnection(interaction.guildId)) {
+      await interaction.reply({ 
+        content: "Hi-Im-DadBot is already in a call.",
+        ephemeral: true,
+      });
+      return
     }
-}
+    await interaction.reply({
+      content: `Joining ${interaction.member?.voice.channel.name}`,
+    });
+    const voiceChannel = interaction.member?.voice.channel;
+
+    const voiceConnection = joinVoiceChannel({
+      channelId: voiceChannel.id,
+      guildId: interaction.guildId,
+      adapterCreator: interaction.guild.voiceAdapterCreator,
+      selfDeaf: false,
+      selfMute: false,
+    });
+  },
+};
