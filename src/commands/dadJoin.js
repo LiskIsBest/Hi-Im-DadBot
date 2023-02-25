8666f313cf3289fce0516ef5c629130bd0709309
Darien Moore <Darienmoore2002@gmail.com>
2023-02-25 02:01:20 -0600

    added black/white list role checking

--- src/commands/dadJoin.js
+++ src/commands/dadJoin.js
@@ -1,11 +1,50 @@
 const { SlashCommandBuilder } = require("discord.js");
 const { joinVoiceChannel, getVoiceConnection } = require("@discordjs/voice");
+const { createMysqlConnection, getListedRoles } = require("../database.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("dadjoin")
     .setDescription("Hi-Im-dad-bot joins the voice call!"),
   async execute(interaction) {
+    const guild_id = interaction.guildId;
+    const member = interaction.member.roles.cache;
+
+    const connection = createMysqlConnection();
+    const whitelisted_roles = await getListedRoles(
+      connection,
+      guild_id,
+      "white"
+    );
+    const blacklisted_roles = await getListedRoles(
+      connection,
+      guild_id,
+      "black"
+    );
+
+    function permCheck(member) {
+      var check = false;
+      for (const role of whitelisted_roles) {
+        if (member.some((r) => r.id === role[1])) {
+          check = true;
+        }
+      }
+      for (const role of blacklisted_roles) {
+        if (member.some((r) => r.id === role[1])) {
+          check = false;
+        }
+      }
+      return check;
+    }
+
+    if (permCheck(member) === false) {
+      await interaction.reply({
+        content: `You do not have permission use this command!`,
+        ephemeral: true,
+      });
+      return;
+    }
+
     if (getVoiceConnection(interaction.guildId)) {
       await interaction.reply({
         content: "Hi-Im-DadBot is already in a call.",
@@ -17,7 +56,7 @@ module.exports = {
 
     const voiceConnection = joinVoiceChannel({
       channelId: voiceChannel.id,
-      guildId: interaction.guildId,
+      guildId: guild_id,
       adapterCreator: interaction.guild.voiceAdapterCreator,
       selfDeaf: false,
       selfMute: false,
--- src/commands/dadLeave.js
+++ src/commands/dadLeave.js
@@ -1,11 +1,50 @@
 const { SlashCommandBuilder } = require("discord.js");
 const { getVoiceConnection } = require("@discordjs/voice");
+const { createMysqlConnection, getListedRoles } = require("../database.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("dadleave")
     .setDescription("Hi-Im-dad-bot leaves the voice call!"),
   async execute(interaction) {
+    const guild_id = interaction.guildId;
+    const member = interaction.member.roles.cache;
+
+    const connection = createMysqlConnection();
+    const whitelisted_roles = await getListedRoles(
+      connection,
+      guild_id,
+      "white"
+    );
+    const blacklisted_roles = await getListedRoles(
+      connection,
+      guild_id,
+      "black"
+    );
+
+    function permCheck(member) {
+      var check = false;
+      for (const role of whitelisted_roles) {
+        if (member.some((r) => r.id === role[1])) {
+          check = true;
+        }
+      }
+      for (const role of blacklisted_roles) {
+        if (member.some((r) => r.id === role[1])) {
+          check = false;
+        }
+      }
+      return check;
+    }
+
+    if (permCheck(member) === false) {
+      await interaction.reply({
+        content: `You do not have permission use this command!`,
+        ephemeral: true,
+      });
+      return;
+    }
+
     const voiceChannel = interaction.member?.voice.channel;
     const voiceConnection = getVoiceConnection(interaction.guildId);
     if (!voiceConnection) {
