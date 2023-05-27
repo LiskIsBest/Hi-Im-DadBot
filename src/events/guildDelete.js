d857f82dc6f3f330244f2c270396842160b885fb
LiskIsBest <liskisbest@gmail.com>
2023-05-26 23:36:51 -0500

    added console log for not finding guild_id

--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -2,7 +2,6 @@ const { Events } = require("discord.js");
 const {
   createMysqlConnection,
   isInServersTable,
-  insertIntoServers,
   updateInServer,
 } = require("../database.js");
 
@@ -12,6 +11,11 @@ module.exports = {
     const guild_id = guild.id
     const connection = createMysqlConnection();
     const in_server = await isInServersTable(connection, guild_id);
+    if (!in_server){
+      connection.end();
+      console.log(`FAILED UPDATE: guild_id not found.`);
+      return
+    }
     await updateInServer(connection, guild_id, false);
     connection.end();
     console.log(`UPDATED servers.in_server TO false FOR guild_id = ${guild_id}`);
